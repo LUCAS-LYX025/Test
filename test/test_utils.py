4650d8b4321e08f5c10fa2140e3d033506a7296c
lei <leiyuxing@zuiyouliao.com>
2025-10-10 11:58:15 +0800

    增加模型

--- test/test_utils.py
+++ test/test_utils.py
@@ -4506,7 +4506,14 @@ elif tool_category == "测试用例生成器":
         with col2:
             model_version = st.selectbox(
                 "模型版本",
-                ["gpt-4", "gpt-4-turbo", "gpt-3.5-turbo"],
+                [
+                    "gpt-4o",
+                    "gpt-4o-mini",
+                    "gpt-4-turbo",
+                    "gpt-4",
+                    "gpt-3.5-turbo",
+                    "gpt-3.5-turbo-16k"
+                ],
                 help="选择GPT模型版本",
                 key="gpt_model_select"
             )
